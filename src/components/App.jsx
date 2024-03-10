0d71a22ea8dffe39dc2e582f588d304d39a2b437
khokenneth31 <khokenneth26@gmail.com>
2024-03-10 12:06:33 +0800

    HW2 Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import React from 'react';
+import FeedbackApp from './FeedbackWidget/FeedbackApp';
+import './styles.css';
+import PhonebookApp from './ContactBook/PhonebookApp';
+
+const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        // fontSize: 40,
+        color: '#010101',
       }}
     >
-      React homework template
+      <PhonebookApp />
+      <FeedbackApp />
     </div>
   );
 };
+
+export default App;
